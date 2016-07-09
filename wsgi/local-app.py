57d6d995f9f3b2aff239e471ead3f096edae09af
Tapan Chandra <tapan.nallan@jda.com>
2016-07-10 03:32:27 +0530

    script to run app locally

--- wsgi/local-app.py
+++ wsgi/local-app.py
@@ -1,5 +1,7 @@
+import bottle
 from bottle import run,TEMPLATE_PATH
 from coursera_podcasts_app import application
 import os 
 
+bottle.debug(True)
 run(application,host='localhost',port=9000,reloader=True)
